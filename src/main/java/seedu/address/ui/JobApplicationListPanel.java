b9c9681a795093b10de03625106b432010e61dc3
san4b0t <sanfo.bt@gmail.com>
2025-10-14 15:37:28 +0800

    Removed unused imports

--- src/main/java/seedu/address/ui/JobApplicationListPanel.java
+++ src/main/java/seedu/address/ui/JobApplicationListPanel.java
@@ -9,7 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.jobapplication.JobApplication;
-import seedu.address.model.person.Person;
 
 /**
  * Panel containing the list of job application.
