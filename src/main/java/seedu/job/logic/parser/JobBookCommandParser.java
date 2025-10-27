3aaf38e5eb765c124642b603c9640a96c50d1204
san4b0t <sanfo.bt@gmail.com>
2025-10-27 17:38:49 +0800

    Add style fixes

--- src/main/java/seedu/job/logic/parser/JobBookCommandParser.java
+++ src/main/java/seedu/job/logic/parser/JobBookCommandParser.java
@@ -7,7 +7,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.job.commons.core.LogsCenter;
-import seedu.job.logic.jobcommands.*;
+import seedu.job.logic.jobcommands.AddJobCommand;
+import seedu.job.logic.jobcommands.Command;
+import seedu.job.logic.jobcommands.DeleteJobCommand;
+import seedu.job.logic.jobcommands.ExitCommand;
+import seedu.job.logic.jobcommands.FilterCommand;
+import seedu.job.logic.jobcommands.FindCommand;
+import seedu.job.logic.jobcommands.ListCommand;
+import seedu.job.logic.jobcommands.SortCommand;
+import seedu.job.logic.jobcommands.TagJobCommand;
+import seedu.job.logic.jobcommands.UntagJobCommand;
+import seedu.job.logic.jobcommands.UpdateJobCommand;
+import seedu.job.logic.jobcommands.HelpCommand;
 import seedu.job.logic.parser.exceptions.ParseException;
 
 /**
